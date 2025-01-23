f6a83f826df6b674c94c4cf3aa86cbdcd080aaba
aballa-learn <aballa.learn@gmail.com>
2025-01-23 17:23:31 -0500

    push pro enabled

--- test_secret.py
+++ test_secret.py
@@ -6,7 +6,7 @@ import aws_lib
 import pymongo
 import pyyaml==5.3
 
-pushpro="disabled"
+pushpro="enabled"
 
 def aws_upload(data: Dict):
     database = aws_lib.connect("AKIAF6BAFJKR46SAWSZ5", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkja")
