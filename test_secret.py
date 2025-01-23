e6dba46026b3b430949d8787561cefac0023464a
aballa-learn <aballa.learn@gmail.com>
2025-01-23 15:48:13 -0500

    added boto3 fake secret

--- test_secret.py
+++ test_secret.py
@@ -1,2 +1,8 @@
-username="user"
-password="admin"
+import boto3
+
+client = boto3.client(
+    's3',
+    aws_access_key_id="AKIAdisnfasdfiondfao",
+    aws_secret_access_key="dfansoeifnasdfmaklsfuhlrnfaweiohc",
+    aws_session_token="dfnawuhchabcasjndjhvkygbnnreaoengsgl"
+)
