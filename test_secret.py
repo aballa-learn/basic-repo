7ea3acc6bcffbdaf9fa8c545dae8ca87565a3133
aballa-learn <aballa.learn@gmail.com>
2025-01-23 16:11:22 -0500

    using mongouri secret

--- test_secret.py
+++ test_secret.py
@@ -2,6 +2,10 @@ import boto3
 import pymongo
 
 MONGO_URI = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority"
+
+def pull_data_from_mongo(query: Dict):
+    return pymongo.connect(MONGO_URI).fetch(query)
+
 client = boto3.client(
     's3',
     aws_access_key_id="AKIAdisnfasdfiondfao",
