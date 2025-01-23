2f1d657acfe0f8609277d576b6b59ee093366d16
aballa-learn <aballa.learn@gmail.com>
2025-01-23 16:57:26 -0500

    copied s3_bucket.py

--- test_secret.py
+++ test_secret.py
@@ -1,22 +1,26 @@
-import boto3
+# Save data to an AWS bucket
+
+from typing import Dict
+import urllib3
+import aws_lib
 import pymongo
+import pyyaml==5.3
+
+def aws_upload(data: Dict):
+    database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ5", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkja")
+    database.push(data)
+
+
+def transform_data(es_data: Dict) -> Dict:
+    es_data = {**data, "origin": "ES"}
 
 MONGO_URI = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority"
 
 def pull_data_from_mongo(query: Dict):
     return pymongo.connect(MONGO_URI).fetch(query)
 
-client = boto3.client(
-    's3',
-    aws_access_key_id="AKIAF6BAFJKR45SAWSZ5",
-    aws_secret_access_key="dfansoeifnasdfmaklsfuhlrnfaweiohc",
-    aws_session_token="dfnawuhchabcasjndjhvkygbnnreaoengsgl"
-)
 
+def push_mongo_to_s3(query):
+    for element in pull_data_from_mongo(query):
+        upload(element)
 
-
-def aws_upload(data: Dict):
-    database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ5", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkja")
-    database.push(data)
-
-vartest=1
